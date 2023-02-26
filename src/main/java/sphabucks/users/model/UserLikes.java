c3fcfc1945d00aa0227044d1bfb35f743bdb8641
seung-hun2 <jungsh2328@naver.com>
2023-02-26 22:50:19 +0900

    userLikes add

---
+++ src/main/java/sphabucks/users/model/UserLikes.java
@@ -0,0 +1,23 @@
+package sphabucks.users.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import sphabucks.products.model.Product;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLikes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private User user;
+    @ManyToOne
+    private Product product;
+}
---
+++ src/main/java/sphabucks/users/service/UserLikesServiceImpl.java
@@ -0,0 +1,38 @@
+package sphabucks.users.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sphabucks.products.model.Product;
+import sphabucks.products.repository.IProductRepository;
+import sphabucks.users.model.UserLikes;
+import sphabucks.users.repository.IUserLikesRepo;
+import sphabucks.users.repository.IUserRepository;
+import sphabucks.users.vo.RequestUserLikes;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserLikesServiceImpl implements IUserLikesService{
+
+    private IUserLikesRepo iUserLikesRepo;
+    private IUserRepository iUserRepository;
+    private IProductRepository iProductRepository;
+    @Override
+    public void addUserLikes(RequestUserLikes requestUserLikes) {
+        iUserLikesRepo.save(UserLikes.builder()
+                        .product(iProductRepository.findById(requestUserLikes.getProductId()).get())
+                        .user(iUserRepository.findById(requestUserLikes.getUserId()).get())
+                .build());
+    }
+
+    @Override
+    public List<UserLikes> getUserLikes(Long userId) {
+        return iUserLikesRepo.findUserLikesByUserId(userId);
+    }
+
+    @Override
+    public List<UserLikes> getAll() {
+        return iUserLikesRepo.findAll();
+    }
+}
