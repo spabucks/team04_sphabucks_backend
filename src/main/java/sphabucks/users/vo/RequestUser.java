a614e3f7ef4ee45477b326548f92308c8927b251
Ji-Wook, Kim <20korea01@gmail.com>
2023-03-02 10:15:35 +0900

    user vo request update

--- src/main/java/sphabucks/users/vo/RequestUser.java
+++ src/main/java/sphabucks/users/vo/RequestUser.java
@@ -1,14 +1,24 @@
 package sphabucks.users.vo;
 
+import jakarta.persistence.Column;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 public class RequestUser {
-
     private String name;
     private String email;
     private String pwd;
+    private String nickname;
+    private String phoneNum;
+    private String address;
+    private Integer star;
+    private String carNum;
+    private Date birth;
+    private boolean sex;    // {true: M, false: F}
+    private String grade;
 
 
 }
