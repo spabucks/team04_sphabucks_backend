0b599f6a9da5cecfed9807d6265e7d287c5d0cb6
Ji-Wook, Kim <20korea01@gmail.com>
2023-03-02 10:32:48 +0900

    purchaseHistory vo request update

--- src/main/java/sphabucks/purchaseHistory/vo/RequestPurchaseHistory.java
+++ src/main/java/sphabucks/purchaseHistory/vo/RequestPurchaseHistory.java
@@ -5,25 +5,14 @@ import lombok.Getter;
 
 @Getter
 public class RequestPurchaseHistory {
-
-    @Column(nullable = false)
     private String category;
-    @Column(nullable = false)
     private String productName;
-    @Column(nullable = false)
     private Long amount;
-    @Column(nullable = false)
     private Long sum;
-    @Column(nullable = false)
     private String payment_num;
-    @Column(nullable = false)
     private Long userId;
-    @Column(nullable = false)
     private Boolean type;
-    @Column(nullable = false)
     private String sp_status;
-    @Column(nullable = false)
     private String or_status;
-    @Column(nullable = false)
     private String image;
 }
