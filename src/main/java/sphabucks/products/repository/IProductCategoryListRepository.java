6e3fb2d75fc08477c244971fbb07c6782423680a
Ji-Wook, Kim <20korea01@gmail.com>
2023-03-03 14:47:35 +0900

    다른고객이 함께본 상품 api 제작중

--- src/main/java/sphabucks/products/repository/IProductCategoryListRepository.java
+++ src/main/java/sphabucks/products/repository/IProductCategoryListRepository.java
@@ -7,4 +7,6 @@ import java.util.List;
 
 public interface IProductCategoryListRepository extends JpaRepository<ProductCategoryList, Long> {
     List<ProductCategoryList> findAllByProductId(Long productId);
+
+    List<ProductCategoryList> findAllBySmallCategoryId(Integer sCategoryId);
 }
