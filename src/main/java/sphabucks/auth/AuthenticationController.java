a0edb8219fb48e08bea7d319be8bd0aad1195daf
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-03-11 16:06:29 +0900

    swagger 적용중

--- src/main/java/sphabucks/auth/AuthenticationController.java
+++ src/main/java/sphabucks/auth/AuthenticationController.java
@@ -1,22 +1,32 @@
 package sphabucks.auth;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sphabucks.users.vo.RequestUser;
 
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
+@Tag(name = "회원 인증", description = "회원가입, 로그인, 로그아웃 등")
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
     @PostMapping("/signup")
+    @Operation(summary = "회원 가입", description = "필수 정보 : loginId, name, email, pwd")
+    @Parameters(value = {
+            @Parameter(name = "loginId", required = true, description = "로그인할 때 사용되는 id"),
+            @Parameter(name = "name", required = true, description = "사용자 이름"),
+            @Parameter(name = "email", required = true, description = "사용자 이메일"),
+            @Parameter(name = "pwd", required = true, description = "로그인 비밀번호")
+    })
     public ResponseEntity<AuthenticationResponse> signup(
+            @RequestParam()
             @RequestBody RequestUser signupRequest) {
         return ResponseEntity.ok(authenticationService.signup(signupRequest));
     }
