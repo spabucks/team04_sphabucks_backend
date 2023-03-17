4d068988579af427a0eef7ef6cc4faeb57dca2fa
seung-hun2 <jungsh2328@naver.com>
2023-03-17 09:20:46 +0900

    Event Error Handler

--- src/main/java/sphabucks/event/service/EventProductServiceImpl.java
+++ src/main/java/sphabucks/event/service/EventProductServiceImpl.java
@@ -2,6 +2,8 @@ package sphabucks.event.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sphabucks.error.BusinessException;
+import sphabucks.error.ErrorCode;
 import sphabucks.event.model.Event;
 import sphabucks.event.model.EventProductList;
 import sphabucks.event.repository.IEventProductListRepository;
@@ -26,10 +28,18 @@ public class EventProductServiceImpl implements IEventProductService{
     private final IEventProductListRepository iEventProductListRepository;
     @Override
     public List<ResponseProductSummary> getProductsByEventId(Long eventId) {
+
         List<ResponseProductSummary> responseProductSummaries = new ArrayList<>();
+
+        if(iEventProductListRepository.findAllByEvent_Id(eventId).isEmpty()){
+            throw new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode());
+        }
+
         List<EventProductList> eventProductLists = iEventProductListRepository.findAllByEvent_Id(eventId);
+
         for (EventProductList eventProductList : eventProductLists) {
-            Product product = iProductRepository.findById(eventProductList.getProduct().getId()).get();
+            Product product = iProductRepository.findById(eventProductList.getProduct().getId())
+                    .orElseThrow(()->new BusinessException(ErrorCode.PRODUCT_NOT_EXISTS, ErrorCode.PRODUCT_NOT_EXISTS.getCode()));
             responseProductSummaries.add(ResponseProductSummary.builder()
                     .id(product.getId())
                     .title(product.getName())
@@ -38,6 +48,7 @@ public class EventProductServiceImpl implements IEventProductService{
                     .imgUrl(iProductImageRepo.findAllByProductId(product.getId()).get(0).getImage())
                     .build());
         }
+
         return responseProductSummaries;
     }
 
@@ -45,15 +56,22 @@ public class EventProductServiceImpl implements IEventProductService{
     public List<ResponseProductList> recommendMD() {
         List<ResponseProductList> responseProductLists = new ArrayList<>();    // 최종 결과로 보내지는 리스트
 
+        if(iEventRepository.findAllByIsRecommendIsTrue().isEmpty()){
+            throw new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode());
+        }
+
         List<Event> recommendEvents = iEventRepository.findAllByIsRecommendIsTrue();    // 추천MD에 해당하는 이벤트 리스트
         recommendEvents.forEach( event -> {
             Long eventId = event.getId();
             responseProductLists.add(ResponseProductList.builder() // 하나의 이벤트에 대한 내용과 상품들이 모두 담길 response
                     .id(eventId)
-                    .name(iEventRepository.findById(eventId).get().getSeason())
+                    .name(iEventRepository.findById(eventId)
+                            .orElseThrow(()-> new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode()))
+                            .getSeason())
                     .data(getProductsByEventId(eventId))
                     .build());
         });
+
         return responseProductLists;
     }
 
@@ -61,14 +79,18 @@ public class EventProductServiceImpl implements IEventProductService{
     public EventProductList addEventProductList(RequestEventProductList requestEventProductList) {
 
         EventProductList eventProductList = EventProductList.builder()
-                .product(iProductRepository.findById(requestEventProductList.getProductId()).get())
-                .event(iEventRepository.findById(requestEventProductList.getEventId()).get())
+                .product(iProductRepository.findById(requestEventProductList.getProductId())
+                        .orElseThrow(()->new BusinessException(ErrorCode.PRODUCT_NOT_EXISTS, ErrorCode.PRODUCT_NOT_EXISTS.getCode())))
+                .event(iEventRepository.findById(requestEventProductList.getEventId())
+                        .orElseThrow(()-> new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode())))
                 .build();
 
         return iEventProductListRepository.save(eventProductList);
     }
     @Override
     public EventProductList getEventProductList(Long id) {
-        return iEventProductListRepository.findById(id).get();
+        EventProductList eventProductList = iEventProductListRepository.findById(id)
+                .orElseThrow(()-> new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode()));
+        return eventProductList;
     }
 }
--- src/main/java/sphabucks/event/service/EventServiceImpl.java
+++ src/main/java/sphabucks/event/service/EventServiceImpl.java
@@ -3,6 +3,8 @@ package sphabucks.event.service;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import sphabucks.error.BusinessException;
+import sphabucks.error.ErrorCode;
 import sphabucks.event.model.Event;
 import sphabucks.event.model.EventImage;
 import sphabucks.event.model.EventProductList;
@@ -30,6 +32,11 @@ public class EventServiceImpl implements IEventService {
 
     @Override
     public void addEvent(RequestEvent requestEvent) {
+
+        if(iEventRepository.findBySeason(requestEvent.getSeason()).isPresent()){
+            throw new BusinessException(ErrorCode.DUPLICATE_EVENT, ErrorCode.DUPLICATE_EVENT.getCode());
+        }
+
         ModelMapper modelMapper = new ModelMapper();
         Event event = modelMapper.map(requestEvent, Event.class);
         iEventRepository.save(event);
@@ -37,18 +44,29 @@ public class EventServiceImpl implements IEventService {
 
     @Override
     public Event getEvent(Long id) {
-        return iEventRepository.findById(id).get();
+
+        Event event = iEventRepository.findById(id)
+                .orElseThrow(()-> new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode()));
+
+        return event;
     }
 
     @Override
     public List<Event> getEventAll() {
+
+        if(iEventRepository.findAll().isEmpty()){
+            throw new BusinessException(ErrorCode.EVENT_NOT_EXISTS, ErrorCode.EVENT_NOT_EXISTS.getCode());
+        }
+
         return iEventRepository.findAll();
     }
 
     @Override
     public EventImage addEventImage(RequestEventImage requestEventImage) {
+
         EventImage eventImage = EventImage.builder()
-                .event(iEventRepository.findById(requestEventImage.getEventId()).get())
+                .event(iEventRepository.findById(requestEventImage.getEventId())
+                        .orElseThrow(()-> new BusinessException(ErrorCode.DUPLICATE_EVENT, ErrorCode.DUPLICATE_EVENT.getCode())))
                 .image(requestEventImage.getImage())
                 .alt(requestEventImage.getAlt())
                 .build();
@@ -59,24 +77,35 @@ public class EventServiceImpl implements IEventService {
     @Override
     public EventImage getEventImage(Long id) {
 
-        return iEventImageRepository.findById(id).get();
+        return iEventImageRepository.findById(id)
+                .orElseThrow(()-> new BusinessException(ErrorCode.IMAGE_NOT_EXISTS, ErrorCode.IMAGE_NOT_EXISTS.getCode()));
     }
 
     @Override
     public List<Event> getRecommendEvent() {
+
+        if(iEventRepository.findAllByIsRecommendIsTrue().isEmpty()){
+            throw new BusinessException(ErrorCode.PRODUCT_NOT_EXISTS, ErrorCode.PRODUCT_NOT_EXISTS.getCode());
+        }
+
         return iEventRepository.findAllByIsRecommendIsTrue();
     }
 
     @Override
     public List<ResponseEventBanner> getEventBanner() {
+
+        if(iEventImageRepository.findAll().isEmpty()){
+            throw new BusinessException(ErrorCode.IMAGE_NOT_EXISTS, ErrorCode.IMAGE_NOT_EXISTS.getCode());
+        }
         List<EventImage> eventImages = iEventImageRepository.findAll();
-        Collections.sort(eventImages, (o1, o2) -> (int) (o2.getEvent().getId() - o1.getEvent().getId()));
+
+        eventImages.sort((o1, o2) -> (int) (o2.getEvent().getId() - o1.getEvent().getId()));
 
         List<ResponseEventBanner> eventList = new ArrayList<>();
 
         eventImages.forEach(eventImage -> {
             if (eventImage.getEvent().getId() != 1) {
-                if (eventImage.getEvent().getIsRecommend() == false) {
+                if (!eventImage.getEvent().getIsRecommend()) {
                     ResponseEventBanner responseEventBanner = ResponseEventBanner.builder()
                             .eventId(eventImage.getEvent().getId())
                             .name(eventImage.getEvent().getSeason())
