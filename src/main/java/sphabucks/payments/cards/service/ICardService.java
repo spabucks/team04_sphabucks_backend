cd7c15fc2d820f78b3cc7cc5e9d1329e1cd7102b
Ju-Yeongmin <drodudals1@gmail.com>
2023-03-23 10:32:58 +0900

    test

--- src/main/java/sphabucks/payments/cards/service/ICardService.java
+++ src/main/java/sphabucks/payments/cards/service/ICardService.java
@@ -6,4 +6,6 @@ import sphabucks.payments.cards.vo.ResponseCard;
 public interface ICardService {
     void addCard(RequestCard requestCard);
     ResponseCard getCard(Long id);
+
+    void test();
 }
