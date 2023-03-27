3c39c3dbb15fc292f32273a13240fdcf2a979cd2
seung-hun2 <jungsh2328@naver.com>
2023-03-27 18:09:25 +0900

    ResponseEntity api

--- src/main/java/sphabucks/domain/payments/gifticons/controller/GiftIconController.java
+++ src/main/java/sphabucks/domain/payments/gifticons/controller/GiftIconController.java
@@ -3,6 +3,8 @@ package sphabucks.domain.payments.gifticons.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import sphabucks.domain.payments.gifticons.vo.RequestGiftIcon;
 import sphabucks.domain.payments.gifticons.vo.ResponseGiftIcon;
@@ -20,13 +22,14 @@ public class GiftIconController {
 
     @PostMapping("/add")
     @Operation(summary = "기프티콘 정보 등록", description = "어드민 권한 - 삭제 예정?")
-    public void addGiftIcon(@RequestBody RequestGiftIcon requestGiftIcon) {
+    public ResponseEntity<Object> addGiftIcon(@RequestBody RequestGiftIcon requestGiftIcon) {
         iGiftIconService.addGiftIcon(requestGiftIcon);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/get/{id}")
     @Operation(summary = "기프티콘 정보 조회", description = "어드민 권한인데 고객이 사용할 가능성도 있음")
-    public ResponseGiftIcon getGiftIcon(@PathVariable Long id) {
-        return iGiftIconService.getGiftIcon(id);
+    public ResponseEntity<Object> getGiftIcon(@PathVariable Long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(iGiftIconService.getGiftIcon(id));
     }
 }
