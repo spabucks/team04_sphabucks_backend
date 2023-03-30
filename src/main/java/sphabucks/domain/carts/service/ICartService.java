00d7a292cfefad9b3b0f15bf1d8b693733cf0739
seung-hun2 <jungsh2328@naver.com>
2023-03-30 09:54:27 +0900

    cart authentication update

--- src/main/java/sphabucks/domain/carts/service/ICartService.java
+++ src/main/java/sphabucks/domain/carts/service/ICartService.java
@@ -1,6 +1,7 @@
 package sphabucks.domain.carts.service;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import sphabucks.domain.carts.vo.*;
 import sphabucks.global.auth.vo.RequestHead;
 
@@ -9,16 +10,16 @@ import java.util.List;
 public interface ICartService {
 
     // userId : User의 UUid
-    ResponseEntity<Object> addCart(RequestHead requestHead, RequestCart requestCart);
+    ResponseEntity<Object> addCart(String userId, RequestCart requestCart);
 
     // uuid 를 입력받음
-    List<ResponseGetCart> getCart(RequestHead requestHead);
+    List<ResponseGetCart> getCart(String userId);
 
     // 카트안의 상품 정보를 불러오는 api
     ResponseGetCartProduct getCartProduct(Long productId);
 
     // 카트의 모든 상품 정보를 한번에 넘기는 api
-    List<ResponseCartV2> getCartV2(RequestHead requestHead);
+    List<ResponseCartV2> getCartV2(String userId);
 
     void updateCart(RequestUpdateCart requestUpdateCart);
 
@@ -26,5 +27,5 @@ public interface ICartService {
 
     void deleteSelectedCart(List<RequestDeleteSelectedCart> requestList);
 
-    void deleteAll(RequestHead requestHead);
+    void deleteAll(String userId);
 }
