bc572ac7c81c0fc1cf8f9c27659ee526cc4cf2ed
seung-hun2 <jungsh2328@naver.com>
2023-04-04 23:04:43 +0900

    get method update

--- src/main/java/sphabucks/domain/users/service/IUserLikesService.java
+++ src/main/java/sphabucks/domain/users/service/IUserLikesService.java
@@ -2,11 +2,12 @@ package sphabucks.domain.users.service;
 
 import sphabucks.domain.users.model.UserLikes;
 import sphabucks.domain.users.vo.RequestUserLikes;
+import sphabucks.domain.users.vo.ResponseUserLikes;
 
 import java.util.List;
 
 public interface IUserLikesService {
     void pushUserLikes(String userId, RequestUserLikes requestUserLikes);
-    List<UserLikes> getUserLikes(String userId);
+    List<ResponseUserLikes> getUserLikes(String userId);
     List<UserLikes> getAll();
 }
